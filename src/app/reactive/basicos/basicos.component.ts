0c073ee1bc64876f4fe32193abf485ce94111409
blank <ander.er985@gmail.com>
2021-07-05 20:58:17 -0500

    segundo commit

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,16 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
+  constructor(private builder: FormBuilder) {}
 
-  constructor() { }
+  /*miformulario: FormGroup = new FormGroup({
+    nombre: new FormControl(),
+    precio: new FormControl(),
+    existencias: new FormControl(),
+  });
+  */
+  miformulario: FormGroup = this.builder.group({
+    nombre: [, [Validators.required, Validators.minLength(3)]],
+    precio: [, [Validators.required, Validators.min(0)]],
+    existencias: [, [Validators.required, Validators.min(0)]],
+  });
 
-  ngOnInit(): void {
+  validarcampo(campo: string) {
+    return (
+      this.miformulario.controls[campo].errors &&
+      this.miformulario.controls[campo].touched
+    );
   }
 
+  guardar() {
+    if (this.miformulario.invalid) {
+      this.miformulario.markAllAsTouched();
+    } else {
+      console.log(this.miformulario.value);
+      this.miformulario.reset();
+    }
+  }
 }
--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,16 +1,76 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
+  /*contructor para llamar al servicio para crear formularios
+    -FormBuilder
+  */
+  constructor(private builder: FormBuilder) {}
 
-  constructor() { }
+  miformulario: FormGroup = this.builder.group({
+    /*
+    Validators para poner condicionales a los campos
+    en el html se les debe asignar el campo con formcontrolname
+    */
+    nombre: [, [Validators.required, Validators.minLength(3)]],
+    //metodo para definir un array con formularios reactivos
+    favoritos: this.builder.array(
+      [['metal gear'], ['wow']],
+      Validators.required
+    ),
+  });
 
-  ngOnInit(): void {
+  // form control para añadir un nuevo valor al array de favoritos
+  //para esto se debe crear por aparte del form group
+  //se le asigna al input con [formcontrol]="nuevofavorito"
+  nuevofavorito: FormControl = this.builder.control('', Validators.required);
+
+  //metodo para validar campos , usando los validators
+  validarcampo(campo: string) {
+    return (
+      this.miformulario.controls[campo].errors &&
+      this.miformulario.controls[campo].touched
+    );
+  }
+  //metodo para guardar y reiniciarl el formulario
+  guardar() {
+    if (this.miformulario.invalid) {
+      this.miformulario.markAllAsTouched();
+    } else {
+      console.log(this.miformulario.value);
+      this.miformulario.reset();
+      this.favoritosarr.clear();
+    }
+  }
+  //metodo para obtener los valores de un array con formularios reactivos
+  get favoritosarr() {
+    return this.miformulario.get('favoritos') as FormArray;
   }
 
+  //metodo para agregar juegos favoritos
+  agregarfavorito() {
+    if (this.nuevofavorito.invalid) {
+      return;
+    } else {
+      this.favoritosarr.push(
+        new FormControl(this.nuevofavorito.value, Validators.required)
+      );
+      this.nuevofavorito.reset();
+    }
+  }
+  //metodo para eliminar un elemento del array de favoritos
+  borrarfavorito(i: number) {
+    this.favoritosarr.removeAt(i);
+  }
 }
